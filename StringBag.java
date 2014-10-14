a1e9a1f1d29988499b4b799ec4bc08d904c7ea8a
trixr4kdz <troque@lion.lmu.edu>
2014-10-14 00:15:44 -0700

    StringBag
    StringBag implements SimpleCollection using ArrayList

--- StringBag.java
+++ StringBag.java
@@ -1,74 +1,67 @@
-/** Class StringBag extends Arraylist and implements SimpleCollection. */
+/** This is a concrete class called StringBag that uses ArrayList and implements the methods in the interface SimpleCollection. */
 
 import java.util.ArrayList;
 
-public class StringBag extends java.util.ArrayList implements SimpleCollection {
+public class StringBag implements SimpleCollection {
 
-    /** StringBag constructor without parameters. */
-    private StringBag () {
-        super ();
-    }
+	private ArrayList < String > stringArr;
 
-	/** StringBag constructor using array of objects as parameter. */
-	private StringBag ( String [] s ) {
-        super ();
+    /** Stringbag constructor using array of strings as parameter. */
+    private StringBag ( String [] s ) {
+        this.stringArr = new ArrayList < String > ( s.length );
+        for ( int i = 0; i < s.length; i++ ) {
+        	this.stringArr.add ( s [ i ] ) ;
+        }
     }
 
 	/** Ensures that this collection contains the specified element (optional operation). */
     public boolean add ( Object o ) {
-        if ( o instanceof String ) {
-    	   return this.add ( ( String ) o );
-        }
-        return false;
+    	boolean changed = false;
+    	if ( o instanceof String ) {
+    		changed = this.stringArr.add ( ( String ) o );
+    	}
+    	return changed;
     }
     
     /** Removes all of the elements from this collection (optional operation). */
-    public void clear () {
-    	this.clear ();
+    public void clear() {
+    	this.stringArr.clear ();
     }
     
     /** Returns true if this collection contains the specified element. */
     public boolean contains ( Object o ) {
-    	return this.contains ( ( String ) o );
+    	return this.stringArr.contains ( ( String ) o );
     }
 
     /** Returns true if this collection contains no elements. */
-   	public boolean isEmpty () {
-    	return this.isEmpty();
+    public boolean isEmpty () {
+    	return this.stringArr.isEmpty();
     }
 
     /** Removes a single instance of the specified element from this collection, if it is present. */
     public boolean remove ( Object o ) {
-        return this.remove ( ( String ) o );
+        return this.stringArr.remove ( ( String ) o );
     }
     
     /** Returns the number of elements in this collection. */
     public int size() {
-       return this.size();
+       return this.stringArr.size();
     }
     
     /** Returns an array containing all of the elements in this collection. */
     public Object[] toArray() {
-        return this.toArray();
+        return this.stringArr.toArray();
     }
 
     /** Returns a string representation of StringBag. */
-  	public String toString () {
+    public String toString () {
     	String result = "";
-    	for ( int i = 0; i < this.size (); i++ ) {
-    		result += "" + this.get ( i );
-    		if ( i < this.size() - 1 ) {
+    	for ( int i = 0; i < this.stringArr.size(); i++ ) {
+    		result += "" + this.stringArr.get ( i );
+    		if ( i < this.stringArr.size() - 1 ) {
                 result += ", ";
             }
     	}
     	return "[" + result + "]";
-    }
-
-    public static void main ( String [] args ) {
-    	StringBag sb = new StringBag ( args );
-    	//sb.clear();
-    	//System.out.println ( sb.toString () );
-    	System.out.println ( sb.isEmpty () );
-    	System.out.println ( sb.add ( "dog" ) );
     }
 }
